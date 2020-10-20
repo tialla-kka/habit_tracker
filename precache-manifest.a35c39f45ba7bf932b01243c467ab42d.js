bd4dc5b4f301e302a47938c34a5b42392626ebcc
tialla-kka <tialla99@hanmail.net>
2020-10-20 14:56:06 +0900

    Updates

---
+++ precache-manifest.a35c39f45ba7bf932b01243c467ab42d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ed31d63df569a3e8ad5ff8f60477b3",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "beb614f04844d3ecbc26",
+    "url": "/habit_tracker/static/css/main.a7c62dc8.chunk.css"
+  },
+  {
+    "revision": "0a137d472f3af448e4b5",
+    "url": "/habit_tracker/static/js/2.c80a0d7e.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit_tracker/static/js/2.c80a0d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb614f04844d3ecbc26",
+    "url": "/habit_tracker/static/js/main.fff9dc1b.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
